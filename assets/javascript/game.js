6e3b839b19ba68d8568309ced93e1dac2bb03eda
Yadira Soto <lunar13heart@hotmail.com>
2019-06-04 17:34:23 -0400

    tweeking my html & js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,17 @@
-// my javascript will go here
+var amethys = 0 ;
+var diamond = 0;
+var citrin = 0;
+var pearl =0;
+
+
+function random() {
+    Math.floor((Math.random() * 100) + 19)
+}
+
+
+// on click
+// click();
+
+// function click() {
+//     <button onclick="random()">amethys</button>;
+// }
