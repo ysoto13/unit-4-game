5066920b0be525f795559ce6faa805f5f8a9a505
Yadira Soto <lunar13heart@hotmail.com>
2019-06-04 19:44:58 -0400

    added ids to h2 text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,5 +7,20 @@ var pearl =0;
 function number() {
      Math.floor(Math.random() * 100) + 19;
      console.log(number);
-}
+};
 
+function myFunction() {
+
+
+
+};
+
+
+
+
+
+
+<button id="button">ClickMe </button>
+
+
+// <button onclick="myFunction()">Click me</button>
