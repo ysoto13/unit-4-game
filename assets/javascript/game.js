811954e3ebfd11d9ca462ab9785f37469866ffac
Yadira Soto <lunar13heart@hotmail.com>
2019-06-05 17:33:43 -0400

    added some comments to my js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,6 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-
 var amethyst = 0;
 var diamond = 0;
 var citrine = 0;
@@ -16,14 +15,20 @@ var loses = 0;
 // random number at start of game between 19-120
 var targetNumber = Math.floor(Math.random() * 120) + 19;
 
+// displays the target number we need to add up to, to win the game
 $("#targetNumber").text(targetNumber);
 
+// displays my score
 $("#scoreTotal").text(yourScore);
 
+// displays my wins
 $("#winsTotal").text(wins);
 
+// displays my loses
 $("#losesTotal").text(loses);
 
+
+// this is what will give my gems a number between 1-12
 amethyst = Math.floor(Math.random() * 12) + 1;
 diamond = Math.floor(Math.random() * 12) + 1;
 citrine = Math.floor(Math.random() * 12) + 1;
@@ -32,8 +37,8 @@ pearl = Math.floor(Math.random() * 12) + 1;
 
 //update gem values on the page
 $("#gem1Value")
-// player clicks gem & points, between 1-12 value, will be added to their score
 
+// player clicks gem & points, between 1-12 value, will be added to their score
 $( "#gem1" ).on( "click", function() {
     console.log("click on gem1");
   });
