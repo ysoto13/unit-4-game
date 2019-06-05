3a1e483ac6c2e2e1caccccaeab1ae52593d46481
Yadira Soto <lunar13heart@hotmail.com>
2019-06-04 22:58:47 -0400

    added notes in js & game rules in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,19 @@
-var amethys = 0 ;
+var amethyst = 0;
 var diamond = 0;
 var citrine = 0;
 var pearl = 0;
 
 
+// random number at start of game between 19-120
 
-var random = Math.floor(Math.random()*101+19)
-var amethys = Math.floor(Math.random()*11+1)
-var diamond = Math.floor(Math.random()*11+1)
-var citrine = Math.floor(Math.random()*11+1)
-var pearl = Math.floor(Math.random()*11+1)
+// player clicks gem & points, between 1-12 value, will be added to their score
 
+// player wins when their number matches the random number at start
 
+// player loses if their score passes the start number
+
+// game restarts when the player wins or loses
+
+// game restarts with a new random number at the top, gem have new values, & players score will reset
+
+// must show how many games the player has won & lost
