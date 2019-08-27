09a37061f82585afcbee3a86ef8384be935ab3d2
Yadira Soto <lunar13heart@hotmail.com>
2019-08-26 22:43:19 -0400

    edited the front end of this game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,10 @@ $( document ).ready(function() {
     console.log( "ready!" );
 
 
-var amethyst = 0;
 var diamond = 0;
+var amethyst = 0;
 var citrine = 0;
-var pearl = 0;
+var sapphire = 0;
 var yourScore = 0;
 var wins = 0;
 var loses = 0;
@@ -29,17 +29,17 @@ $("#losesTotal").text(loses);
 
 
 // this is what will give my gems a number between 1-12
-amethyst = Math.floor(Math.random() * 11 + 1);
 diamond = Math.floor(Math.random() * 11 + 1);
+amethyst = Math.floor(Math.random() * 11 + 1);
 citrine = Math.floor(Math.random() * 11 + 1);
-pearl = Math.floor(Math.random() * 11 + 1);
+sapphire = Math.floor(Math.random() * 11 + 1);
 
 
 
 // player clicks gem & points, between 1-12 value, will be added to their score
 $("#gem1").on("click", function() {
-    console.log(amethyst + yourScore);
-    yourScore = yourScore + amethyst;
+    console.log(diamond + yourScore);
+    yourScore = yourScore + diamond;
     $("#scoreTotal").text(yourScore);
     if (yourScore == targetNumber){
       winner();
@@ -49,8 +49,8 @@ $("#gem1").on("click", function() {
   });
 
   $("#gem2").on("click", function() {
-    console.log(diamond + yourScore);
-    yourScore = yourScore + diamond;
+    console.log(amethyst + yourScore);
+    yourScore = yourScore + amethyst;
     $("#scoreTotal").text(yourScore);
     if (yourScore == targetNumber){
       winner();
@@ -73,7 +73,7 @@ $("#gem1").on("click", function() {
   });
 
   $("#gem4").on("click", function(){
-    yourScore = yourScore + pearl;
+    yourScore = yourScore + sapphire;
     $("#scoreTotal").text(yourScore);
     if (yourScore == targetNumber){
       winner();
@@ -97,10 +97,10 @@ function loser(){
  function reset(){
   $("#targetNumber").text(targetNumber);
   targetNumber = Math.floor(Math.random() * 101 + 19);
-  amethyst = Math.floor(Math.random() * 11 + 1);
   diamond = Math.floor(Math.random() * 11 + 1);
+  amethyst = Math.floor(Math.random() * 11 + 1);
   citrine = Math.floor(Math.random() * 11 + 1);
-  pearl = Math.floor(Math.random() * 11 + 1);
+  sapphire = Math.floor(Math.random() * 11 + 1);
   yourScore = 0;
   $("#scoreTotal").text(yourScore);
 };
